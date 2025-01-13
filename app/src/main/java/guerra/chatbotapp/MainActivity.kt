b46a18a24883081d61744cdb09a2801f9e3f6da8
RafaelGuerraE <gueerae@gmail.com>
2025-01-13 17:41:36 -0300

    Initial Commit

---
+++ app/src/main/java/guerra/chatbotapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package guerra.chatbotapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import guerra.chatbotapp.ui.theme.ChatbotAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ChatbotAppTheme {
+                Surface(modifier = Modifier.fillMaxSize().windowInsetsPadding(WindowInsets.statusBars)) {
+                    
+                }
+            }
+        }
+    }
+}
